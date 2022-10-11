9205f920124668c386fdbda446140d88da4066f3
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-10-11 16:47:57 +0600

    Header banner section complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home Page</h2>
+        <div className='bg-lime-500'>
+            <div className='px-4 py-20 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"'>
+                <h2 className='text-white text-5xl font-bold'>Start The Questionnaire Answer</h2>
+                <p className='mt-5 text-white text-lg'>A questionnaire is a research instrument that consists of a set of questions or other types of prompts that aims to collect information from a respondent. A research questionnaire is typically a mix of close-ended questions and open-ended questions.</p>
+            </div>
         </div>
     );
 };
